d872efd02cdc9ccdb89d59fa056e7a4bb56f57da
scg130 <scg130@163.com>
2024-03-20 04:09:57 +0000

    1

--- mysql.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --name mysql8.0 -e MYSQL_ROOT_PASSWORD=smd013012 -d -p 3306:3306 mysql:latest --lower-case-table-names=1
-docker run -d --name myredis -p 6379:6379 redis --requirepass "smd013012"
